b875e2448ecf31b8b0bc460ea7cdab596900854d
Taha <valnomiry@gmail.com>
2026-02-24 13:12:49 +0300

    .

--- OppAssignment3/Cinema.cs
+++ OppAssignment3/Cinema.cs
@@ -3,51 +3,7 @@ using OppAssignment3;
 
 namespace OppAssignment2
 {
-    internal class Cinema1
-    {
-        private Ticket[] tickets = new Ticket[20];
-
-        // Indexer with validation as required
-        public Ticket this[int index]
-        {
-            get
-            {
-                if (index >= 0 && index < tickets.Length)
-                    return tickets[index];
-                return null;
-            }
-            set
-            {
-                if (index >= 0 && index < tickets.Length)
-                    tickets[index] = value;
-                // else do nothing
-            }
-        }
-
-        public Ticket GetMovieByName(string movieName)
-        {
-            foreach (var t in tickets)
-            {
-                if (t != null && t.MovieName.Equals(movieName, StringComparison.OrdinalIgnoreCase))
-                    return t;
-            }
-            return null;
-        }
-
-        public bool AddTicket(Ticket t)
-        {
-            for (int i = 0; i < tickets.Length; i++)
-            {
-                if (tickets[i] == null)
-                {
-                    tickets[i] = t;
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
-
+    
 
     public class Cinema
     {
